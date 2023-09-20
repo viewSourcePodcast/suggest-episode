55f2c90275b950c005c21edb0912f3a7deace851
Brian Coords <bacoords@gmail.com>
2023-09-19 20:43:59 -0700

    adds suggestion model migration seeder and controller

---
+++ database/seeders/SuggestionSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Suggestion;
+
+class SuggestionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Suggestion::create([
+            'suggestion' => 'Make an episode about Laravel.',
+            'user_id' => 1,
+            'likes' => 0,
+        ]);
+
+        Suggestion::create([
+            'suggestion' => 'Finish that accordion block.',
+            'user_id' => 1,
+            'likes' => 3,
+        ]);
+
+        Suggestion::create([
+            'suggestion' => 'An episode about using GraphQL with WordPress.',
+            'user_id' => 1,
+            'likes' => 1,
+        ]);
+    }
+}
