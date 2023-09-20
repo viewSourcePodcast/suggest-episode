c3a2150fdb7862e801e238c9c100013a76a6bc4d
Brian Coords <bacoords@gmail.com>
2023-09-19 21:01:14 -0700

    sets up basic suggestion view on home page

--- app/Http/Controllers/SuggestionController.php
+++ app/Http/Controllers/SuggestionController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Suggestion;
 use Illuminate\Http\Request;
 
 class SuggestionController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $suggestions = Suggestion::orderBy('likes', 'desc')->get();
+        return view('welcome', ['suggestions' => $suggestions]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SuggestionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SuggestionController::class, 'index'])->name('suggestions.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +27,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
